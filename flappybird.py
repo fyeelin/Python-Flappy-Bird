d8845481d9c871529e16190a50b209396e8105c9
mynameyeelin <yeelin333@gmail.com>
2021-03-27 10:20:40 +0800

    created:flappybbird.py and uploaded imgs
    create a flappybird.py file and uploaded the material image for this game

---
+++ flappybird.py
@@ -0,0 +1,21 @@
+import pygame
+import sys  # pylint: disable=unused-import
+import random  # pylint: disable=unused-import
+
+
+def main():  # pylint: disable=c0116
+    screen = pygame.display.set_mode((288, 512), 0, 32)
+    background = pygame.image.load(
+        r"D:\python_thing\flapy bird\game soure material\bg.png")
+    screen.blit(background, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
